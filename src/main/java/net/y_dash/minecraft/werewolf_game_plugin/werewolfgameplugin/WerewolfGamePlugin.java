c5ae11ac4f5bb075bdeea42c3a4e2c91fd26bed5
Y-dash <yokokura-yusuke@y-dash.net>
2020-07-31 04:12:19 +0900

    init project

---
+++ src/main/java/net/y_dash/minecraft/werewolf_game_plugin/werewolfgameplugin/WerewolfGamePlugin.java
@@ -0,0 +1,17 @@
+package net.y_dash.minecraft.werewolf_game_plugin.werewolfgameplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WerewolfGamePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
