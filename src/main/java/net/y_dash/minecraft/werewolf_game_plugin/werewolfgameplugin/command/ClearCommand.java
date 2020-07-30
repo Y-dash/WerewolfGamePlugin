98b35ef5093eed75fdf232c786897b2287071cb5
Y-dash <yokokura-yusuke@y-dash.net>
2020-07-31 07:14:57 +0900

    fix: 参加者clearコマンド エラー処理追加

--- src/main/java/net/y_dash/minecraft/werewolf_game_plugin/werewolfgameplugin/command/ClearCommand.java
+++ src/main/java/net/y_dash/minecraft/werewolf_game_plugin/werewolfgameplugin/command/ClearCommand.java
@@ -1,6 +1,7 @@
 package net.y_dash.minecraft.werewolf_game_plugin.werewolfgameplugin.command;
 
 import net.y_dash.minecraft.werewolf_game_plugin.werewolfgameplugin.Game;
+import net.y_dash.minecraft.werewolf_game_plugin.werewolfgameplugin.game_status.None;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -23,8 +24,13 @@ public class ClearCommand extends WerewolfGameCommand {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(!(game.gameStatus instanceof None)) {
+            sender.sendMessage("ゲーム開始後には削除できません");
+            return false;
+        }
+
         game.registrantList.clear();
         sender.sendMessage("参加登録リストを全削除しました");
-        return false;
+        return true;
     }
 }
